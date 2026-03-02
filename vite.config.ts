daeada9eab99a63afd5ef9f851862eb3fba98d39
nive-dev <nivethithakesavan7823@gmail.com>
2026-03-02 13:31:56 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/Sri-ramana-patham-residency/",
+  base: "/",
     plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
